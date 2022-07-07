d65c8e51d2bd35c859e9678649eed864184fc3c4
Zachary Baca <bacazach@icloud.com>
2022-07-06 21:36:20 -0500

    added event listener to show/hide job role field

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // Automatically Provides Focus Point To Input Field
 let input = document.querySelector('input');
 input.focus();
+
+// Hide Job Role Field When Form Loads
+let otherJobRole = document.getElementById('other-job-role');
+otherJobRole.style.display = 'none';
+
+// Show Job Role Field If Other Is Selected
+let jobRole = document.getElementById('title');
+jobRole.addEventListener('change', (e) => {
+    if (e.target.value === "other") {
+        otherJobRole.style.display = 'block';
+    } else {
+        otherJobRole.style.display = 'none';
+    }
+})
