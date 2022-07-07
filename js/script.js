240626ab748c4f9df942fb3fbec3a98045a17308
Zachary Baca <bacazach@icloud.com>
2022-07-07 00:14:01 -0500

    added loop to hide select options

--- js/script.js
+++ js/script.js
@@ -15,3 +15,25 @@ jobRole.addEventListener('change', (e) => {
         otherJobRole.style.display = 'none';
     }
 })
+
+// Design Select Element Should Listen For Changes, And Only Show Options According To What Is Selected
+let colorElement = document.getElementById('color');
+let designElement = document.getElementById('design');
+let colorOptions = colorElement.options;
+console.log(colorOptions);
+// Disables Color Select Element From Being Selectable
+colorElement.disabled = true;
+// Event Enables colorElement If Selection Is Made On Design Element
+designElement.addEventListener('change', (e) => {
+    colorElement.disabled = false;
+    for (let i = 1; i < colorOptions.length; i++) {
+        if (e.target.value === 'js puns') {
+            if (colorOptions[i].dataset.theme !== 'js puns') {
+                colorOptions[i].setAttribute("hidden", "hidden");
+            }
+        }
+        else {
+            colorOptions[i].removeAttribute("hidden");
+        }
+    }
+})
